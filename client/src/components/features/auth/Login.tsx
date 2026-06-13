631c65ca9ddaf067d9789d62470c00df3629c42b
Kamyar Azizi <kamyar2019azizi@gmail.com>
2026-06-13 18:31:04 +0200

    feat: added login and session management in client feat: added favicon

--- client/src/components/features/auth/Login.tsx
+++ client/src/components/features/auth/Login.tsx
@@ -1,14 +1,41 @@
 import {Link} from "react-router";
 import StationElement from "../../ui/StationElement";
+import {useState} from "react";
+import {logIn} from "../../../api/auth";
 
-export default function Login() {
+type Props = {
+  setIsLoggedIn: React.Dispatch<React.SetStateAction<boolean>>;
+};
+
+export default function Login({setIsLoggedIn}: Props) {
+  const [username, setUsername] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+  const [error, setError] = useState<string | null>(null);
+  const [loading, setLoading] = useState(false);
+
+  const handleLogin = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    try {
+      setLoading(true);
+      setError(null);
+      await logIn({username, password});
+      setIsLoggedIn(true);
+    } catch (err) {
+      setError(
+        err instanceof Error ? err.message : "An unexpected error occurred.",
+      );
+    } finally {
+      setLoading(false);
+    }
+  };
+
   return (
     <div className="min-h-screen w-full px-12 relative overflow-hidden">
       <StationElement position="left" />
       <StationElement position="right" />
       <h1 className="text-center pt-6">LAST RUN</h1>
       <form
-        action=""
+        onSubmit={handleLogin}
         className="flex  text-center flex-col gap-4 mt-12 w-full max-w-md mx-auto justify-center border rounded-2xl p-6 shadow-lg"
       >
         <h2>Login</h2>
@@ -16,16 +43,20 @@ export default function Login() {
           className="p-2 rounded border w-full"
           type="text"
           placeholder="Username"
+          onChange={(e) => setUsername(e.target.value)}
         />
         <input
           className="p-2 rounded border w-full"
           type="password"
           placeholder="Password"
+          onChange={(e) => setPassword(e.target.value)}
         />
+        {error && <p className="text-red-500">{error}</p>}
         <input
-          className="p-2 rounded cursor-pointer mt-3 text-white bg-indigo-900 hover:bg-indigo-800 duration-150 w-full"
+          className="p-2 disabled:cursor-not-allowed disabled:opacity-50 disabled:hover:bg-indigo-900 rounded cursor-pointer mt-3 text-white bg-indigo-900 hover:bg-indigo-800 duration-150 w-full"
           type="submit"
           value="Login"
+          disabled={loading || !username || !password}
         />
         <Link
           to="/instructions"
--- client/src/components/features/auth/Router.tsx
+++ client/src/components/features/auth/Router.tsx
@@ -8,22 +8,46 @@ import InstructionsPage from "../instructions/InstructionsPage";
 import WrongRoutePage from "./WrongRoutePage";
 import ExecutionPage from "../game/execution-phase/ExecutionPage";
 import ResultPage from "../game/result-phase/ResultPage";
+import {useEffect, useState} from "react";
+import {checkSession} from "../../../api/auth";
+import Loading from "../../ui/Loading";
 
 export default function Router() {
-  //   const isLoggedIn = document.cookie.includes("token=");
-  const isLoggedIn = true;
+  const [isLoggedIn, setIsLoggedIn] = useState<boolean>(false);
+  const [loading, setLoading] = useState<boolean>(false);
 
-  if (!isLoggedIn) {
-    return (
-      <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route path="/instructions" element={<InstructionsPage />} />
-        <Route path="*" element={<Navigate to="/login" replace />} />
-      </Routes>
-    );
-  }
+  const checkUserSession = async () => {
+    setLoading(true);
+    try {
+      const user = await checkSession();
+      if (!user) {
+        setIsLoggedIn(false);
+        return;
+      }
+      setIsLoggedIn(true);
+    } catch (err) {
+      console.log(err);
+      setIsLoggedIn(false);
+    } finally {
+      setLoading(false);
+    }
+  };
 
-  return (
+  useEffect(() => {
+    checkUserSession();
+  }, []);
+
+  return loading ? (
+    <div className="w-full h-full flex justify-center items-center">
+      <Loading />
+    </div>
+  ) : !isLoggedIn ? (
+    <Routes>
+      <Route path="/login" element={<Login setIsLoggedIn={setIsLoggedIn} />} />
+      <Route path="/instructions" element={<InstructionsPage />} />
+      <Route path="*" element={<Navigate to="/login" replace />} />
+    </Routes>
+  ) : (
     <Routes>
       <Route path="/login" element={<Navigate to="/home" replace />} />
       <Route path="/" element={<Navigate to="/home" replace />} />
